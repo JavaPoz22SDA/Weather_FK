147aa1e50dd7617c022435492a5a5653663613ae
f_kazmierczak <kazmierczakf@gmail.com>
2020-04-26 10:10:40 +0200

    replace and try-catch

--- src/main/java/pl/sda/weather/WeatherService.java
+++ src/main/java/pl/sda/weather/WeatherService.java
@@ -7,14 +7,13 @@ import java.net.URL;
 
 public class WeatherService {
     private String finalURL;
-    private Weather weather;
     ObjectMapper mapper = new ObjectMapper();
 
     public WeatherService(String url, String key) {
         finalURL = url + "?access_key=" + key;
     }
     public Weather getCityWeather(String city) throws IOException {
-        finalURL = finalURL + "&query=" + city;
+        finalURL = finalURL + "&query=" + city.replace(" ", "%20");
         return mapper.readValue(new URL(finalURL), Weather.class);
     }
 }
