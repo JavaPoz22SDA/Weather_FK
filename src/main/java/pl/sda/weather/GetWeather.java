54a4ef1504a60b62a068ff9e4235d6305ef73c31
f_kazmierczak <kazmierczakf@gmail.com>
2020-04-26 12:46:23 +0200

    optional and jar

--- src/main/java/pl/sda/weather/GetWeather.java
+++ src/main/java/pl/sda/weather/GetWeather.java
@@ -1,8 +1,10 @@
 package pl.sda.weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class GetWeather {
@@ -10,22 +12,17 @@ public class GetWeather {
     public static void main(String[] args) throws IOException {
 
         String API_KEY = "a7c4fbeed5abd6ca490b193fab5d7651";
-
-      /*  ObjectMapper mapper = new ObjectMapper();
-        String url = "http://api.weatherstack.com/current?access_key=a7c4fbeed5abd6ca490b193fab5d7651&query=New%20York";
-
-        Weather object = mapper.readValue(new URL(url), Weather.class);
-        System.out.println(object.getCurrent().getTemperature());*/
-
+        ObjectMapper mapper = new ObjectMapper();
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Podaj miejscowśc bez polskich znaków");
 
         WeatherService weatherService = new WeatherService("http://api.weatherstack.com/current", API_KEY);
         String city = scan.nextLine();
 
-        Weather weather = weatherService.getCityWeather(city);
-        if(weather != null){
-            System.out.println("Temperatura w " + weather.getLocation().getName() + " wynosi: " + weather.getCurrent().getTemperature());
-        }
+        Optional<Weather> weather = weatherService.getCityWeather(city);
+
+        weather.ifPresent(weather1 -> System.out.println("Temperatura w " + weather1.getLocation().getName() + " wynosi: " + weather1.getCurrent().getTemperature()));
+
     }
 }
--- src/main/java/pl/sda/weather/WeatherService.java
+++ src/main/java/pl/sda/weather/WeatherService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 public class WeatherService {
     private String finalURL;
@@ -13,16 +14,19 @@ public class WeatherService {
     public WeatherService(String url, String key) {
         finalURL = url + "?access_key=" + key;
     }
-    public Weather getCityWeather(String city) throws IOException {
-       try {
-           finalURL = finalURL + "&query=" + city.replace(" ", "%20");
-           return mapper.readValue(new URL(finalURL), Weather.class);
-
-       }catch (UnrecognizedPropertyException e){
+    public Optional<Weather> getCityWeather(String city) throws IOException {
+        finalURL = finalURL + "&query=" + city.replace(" ", "%20");
+        Weather weather;
+        Optional<Weather> opt;
+        try {
+            weather = mapper.readValue(new URL(finalURL), Weather.class);
+            opt = Optional.of(weather);
+            return opt;
+        }catch (UnrecognizedPropertyException e){
             ErrorException errorException = mapper.readValue(new URL(finalURL), ErrorException.class);
             System.out.println("Podane miasto nie istnieje. ");
             System.out.println("Błąd: " + errorException.getError().getCode() + " Info " + errorException.getError().getInfo());
-            return null;
+            return Optional.empty();
        }
     }
 }
